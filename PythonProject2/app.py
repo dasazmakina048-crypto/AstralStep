b420d2add7dd78d8f2e19650e8c70d54b96a55e0
dasazmakina048-crypto <dasazmakina048@gmail.com>
2025-11-28 22:54:52 +0500

    Update app.py

--- PythonProject2/app.py
+++ PythonProject2/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -13,19 +12,41 @@ def close_db_connection(conn):
 
 def init_db():
     conn = get_db_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL)')
     conn.close()
 
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
+
+tasks = [
+    {'id': 1, 'title': '', 'done': False},
+    {'id': 2, 'title': '', 'done': True}
+]
 
 
-@app.before_first_request
-def before_first_request():
-    init_db()
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = next((task for task in tasks if task['id'] == task_id), None)
+    if task is None:
+        return jsonify({'error': 'Задача не найдена'}), 404
+    return jsonify({'task': task})
+
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    if not request.json or 'title' not in request.json:
+        return jsonify({'error': 'Некорректный запрос'}), 400
+
+    task = {
+        'id': len(tasks) + 1,
+        'title': request.json['title'],
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
